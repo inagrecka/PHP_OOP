4e619e990f81a30678b25dd82b0b7e8d360d9ccb
LAPTOP-7RR447IQ\tatu8 <tatu89@gmail.com>
2023-01-24 10:58:47 +0200

    bandymas

--- index.php
+++ index.php
@@ -1,30 +1,8 @@
 <?php
 declare(strict_types=1);
 
-/*
-Sukurkite klasę, kuri skaičiuotų keturkampio plotą, perimetrą ir įstrižainę.
-Klasės pavadinimas: Rectangle
-Į konstruktorių paduodama: int $length, int $width
-Metodai:
-- calculateArea()
-- calculatePerimeter()
-- calculateDiagonal()
-Metodai turi grąžinti iki 2 skaitmenų po kablelio į viršų suapvalintą float tipo reikšmę. Pridėkite return tipą metodams.
-*/
-class Rectangle {
-    public int $length;
-    public int $width;
-    public function __construct(int $length, int $width) {
-        $this->length = $length;
-        $this->width = $width;
-    }
-    public function calculateArea(): float {
-        return $this->length*$this->width;
-    }
-    public function calculatePerimeter():float {
-        return (2*$this->length) + (2*$this->width);
-    }
-}
+require 'Rectangle.php';
+
 $rectangleOne = new Rectangle(5, 2);
 var_dump($rectangleOne->calculateArea());
 var_dump($rectangleOne->calculatePerimeter());
